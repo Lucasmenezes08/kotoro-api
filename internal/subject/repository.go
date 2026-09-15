a8b1b409c0674569941ea334cb82269ce5deafb4
Lucasmenezes08 <lms4@cesar.school>
2026-09-15 15:39:11 -0300

    feat: provisory getters

--- internal/subject/repository.go
+++ internal/subject/repository.go
@@ -42,6 +42,16 @@ func (s *SubjectRepository) GetAll(ctx context.Context) ([]Subject, error) {
 	return subject, nil
 }
 
+func (s *SubjectRepository)GetByName(ctx context.Context, name string) (*Subject, error){
+	query := "SELECT id,name,color,created_at,updated_at,deleted_at FROM subjects WHERE deleted_at IS NULL AND name = ?"
+	
+	if err := s.db.SelectContext(ctx, name, query); err != nil{
+		return nil,fmt.Errorf("Error to get subject by name, error, %w", err)
+	}
+
+	return &Subject{},nil
+}
+
 func (s *SubjectRepository) Create(ctx context.Context, subject SubjectCreateInput) error {
 	_, err := s.db.NamedExecContext(ctx, "INSERT INTO subjects (name ,color) VALUES (:name, :color)", &subject)
 
